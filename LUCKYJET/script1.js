cc0b367210e59ec9536238a787cda5c98f9db2ec
MinesGuruHack <mjackpot47@gmail.com>
2024-10-03 14:26:46 +0500

    Update script1.js

--- LUCKYJET/script1.js
+++ LUCKYJET/script1.js
@@ -5,7 +5,7 @@ function getRan(min, max) {
 }
 
 async function checkSignal() {
-    let randomNumber1 = getRan(1.7, 2.1).toFixed(2);
+    let randomNumber1 = getRan(1.2, 1.8).toFixed(2);
     const url = 'https://lucky-jet-history.gamedev-atech.cc/public/history/api/history/replay';
     const response = await fetch(url);
     const data = await response.json();
